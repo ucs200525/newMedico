15196b8f60f2f832a371508c6a110baa076a8574
ucs200525 <chandrasekharupadhyayula@gmail.com>
2024-11-08 23:02:31 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,7 @@ const app = express();
 
 // Enable CORS for all routes
 const front = ['https://new-medico-cukslsvdh-ucs-projects-765a2e85.vercel.app', 'http://localhost:3000'];
-app.use(cors({
-  origin:  '*', // Frontend domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  credentials: true, // Enable sending cookies with cross-origin requests
-}));
-
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("Server Running Successful");
