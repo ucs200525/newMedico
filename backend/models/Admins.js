bae2313dd7c1a761d65ad9308ae84c8085495d68
ucs200525 <chandrasekharupadhyayula@gmail.com>
2024-11-11 20:07:17 +0530

    changes token

--- backend/models/Admins.js
+++ backend/models/Admins.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
-
+userSchema.index({ email: 1 });
 
 // Create the User model
 const Admins = mongoose.model('Admins', userSchema);
